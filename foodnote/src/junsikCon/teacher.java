5e6d79773aa2cfdec6e8651460310713e019d95a
SeoJunsik <SeoJunsik@DESKTOP-EP67A8A>
2021-07-29 17:58:54 +0900

    7.29일자

---
+++ foodnote/src/junsikCon/teacher.java
@@ -0,0 +1,30 @@
+package junsikCon;
+
+public class teacher {
+
+	//변수명 영어로 줄일땐 영어 자음만 줄여서
+	//string.format 으로공백 할 필요 없이 문자열 자체에 공백을 같이 넣을수 있다
+	//자바 기술을 알아보자
+	
+	//다이아몬드 같은 경우엔 똑같은 for문 줄이는걸생각하자
+	//공백 출력 메소드만 따로 거꾸로 메소드 따로 출력 따ㅑ로
+	//그렇게 for문에 적용시키면굿
+	
+	//java.utill도 알아보자
+	//개발용어에 익숙해지자
+	//입력받게하지 생성자는 잘 안쓴다
+	
+	//같은 클래스 내에서 쓰는 메소드는 this.를 써보자(나도 남도 쉽게 알수있게)
+	//print utills
+	//String utills
+	//utill 만들어진게 많으니 구경해보자
+	//PrintOption,PrintUtills
+	
+	//정말 큰 숫자는 bicdecimer 에 intvalue를 쓰면 integer 가 나온다
+	//new BigDecimal(charToStr).intValue() 를 쓰면 저절로 바꿔준다
+
+	//배열 1 , 58
+	//스트링 43, 
+	
+	
+}
